f76b29718ef10784eed8181612784b47c9a290b5
m.morshinin <m.morshinin@b2b-center.ru>
2021-01-15 22:16:19 +0300

    main файлы для обучения

--- tetris.js
+++ tetris.js
@@ -28,7 +28,21 @@ function drawMatrix(matrix, offset) {
     });
 }
 
-function update() {
+let dropCounter = 0;
+let dropInterval = 1000;
+
+let lastTime = 0;
+
+function update(time = 0) {
+    const deltaTime = time - lastTime;
+    lastTime = time;
+    dropCounter += deltaTime;
+
+    if (dropCounter > dropInterval) {
+        player.pos.y++;
+        dropCounter = 0;
+    }
+
     draw();
     requestAnimationFrame(update);
 }
